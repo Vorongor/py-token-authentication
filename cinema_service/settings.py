b6107b33ca7c22ad21542a71d87bbe4cbc2e8b94
Vorongor <mr.maddarknes@gmail.com>
2026-01-03 16:36:43 +0200

    Fix mistake with password length

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation."
                 "CommonPasswordValidator",
+        'OPTIONS': {
+            'min_length': 5,
+        }
     },
     {
         "NAME": "django.contrib.auth.password_validation."
